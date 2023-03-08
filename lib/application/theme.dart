d6f6c44dad6aa44ed8dc1ffacdfad955b71879e4
SF97 <a.uchaev97@gmail.com>
2023-03-08 20:20:40 +0500

    add common element in the theme

--- lib/application/theme.dart
+++ lib/application/theme.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const borderColor = Color(0xff9b9b9b);
+
   static final theme = ThemeData(
       // TODO: later
       //fontFamily: GoogleFonts.roboto().fontFamily,
       );
+
+  static final boxBorder = BoxDecoration(
+    borderRadius: BorderRadius.circular(4),
+    border: Border.all(color: borderColor),
+  );
 
   AppTheme._();
 }
