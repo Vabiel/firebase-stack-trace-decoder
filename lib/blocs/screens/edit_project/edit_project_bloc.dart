862ce324347fb38243f9f13c03a100bddf75548d
SF97 <a.uchaev97@gmail.com>
2023-03-08 20:22:48 +0500

    bloc error fix

--- lib/blocs/screens/edit_project/edit_project_bloc.dart
+++ lib/blocs/screens/edit_project/edit_project_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_stacktrace_decoder/application/extensions/bloc_extension/bloc_extension.dart';
 import 'package:firebase_stacktrace_decoder/models/models.dart';
 import 'package:firebase_stacktrace_decoder/repositories/repositories.dart';
 import 'package:flutter/cupertino.dart';
@@ -17,12 +18,18 @@ class EditProjectBloc extends Bloc<EditProjectEvent, EditProjectState> {
     required this.projectLocalProvider,
     required this.input,
   }) : super(const EditProjectInitial()) {
-    on<EditProjectSavePressed>(_saveProject);
-    on<EditProjectDeletePressed>(_deleteProject);
+    onBlocEvent(_eventToState);
   }
 
-  Future<void> _saveProject(
-      EditProjectSavePressed event, Emitter<EditProjectState> emit) async {
+  Stream<EditProjectState> _eventToState(EditProjectEvent event) async* {
+    if (event is EditProjectSavePressed) {
+      yield* _saveProject(event);
+    } else if (event is EditProjectDeletePressed) {
+      yield* _deleteProject(event);
+    }
+  }
+
+  Stream<EditProjectState> _saveProject(EditProjectSavePressed event) async* {
     final name = event.name;
     final version = event.version;
     final platforms = event.platforms;
@@ -50,22 +57,22 @@ class EditProjectBloc extends Bloc<EditProjectEvent, EditProjectState> {
     }
     try {
       await projectLocalProvider.save(updatedProject);
-      emit(const EditProjectActionComplete(ActionResult.saveSuccess));
+      yield const EditProjectActionComplete(ActionResult.saveSuccess);
     } catch (error) {
       debugPrint('save error: $error');
-      emit(const EditProjectActionComplete(ActionResult.saveFailed));
+      yield const EditProjectActionComplete(ActionResult.saveFailed);
     }
   }
 
-  Future<void> _deleteProject(
-      EditProjectDeletePressed event, Emitter<EditProjectState> emit) async {
+  Stream<EditProjectState> _deleteProject(
+      EditProjectDeletePressed event) async* {
     try {
       final uid = event.projectUid;
       await projectLocalProvider.deleteByUid(uid);
-      emit(const EditProjectActionComplete(ActionResult.deleteSuccess));
+      yield const EditProjectActionComplete(ActionResult.deleteSuccess);
     } catch (error) {
       debugPrint('delete error: $error');
-      emit(const EditProjectActionComplete(ActionResult.deleteFailed));
+      yield const EditProjectActionComplete(ActionResult.deleteFailed);
     }
   }
 }
