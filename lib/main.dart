d9d296d7a72e7f2e709896d2dfdbf60098b9fc6f
SF97 <a.uchaev97@gmail.com>
2023-03-08 20:57:35 +0500

    remove Phoenix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_stacktrace_decoder/application/app.dart';
-import 'package:firebase_stacktrace_decoder/application/di_initializer.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 void main() async {
-  runApp(Phoenix(child: DiInitializer(child: FirebaseStacktraceDecoder())));
+  runApp(FirebaseStacktraceDecoder());
 }
